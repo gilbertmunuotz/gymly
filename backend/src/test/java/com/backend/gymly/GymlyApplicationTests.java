b023ba8d9322b0f41d645e77d9a947325827ed11
gilbertmunuotz <officialgilbert24@gmail.com>
2026-05-28 18:43:33 +0300

    initial commit

---
+++ backend/src/test/java/com/backend/gymly/GymlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.gymly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
